d3a28cef1be115bc48544c362096e13a963ba240
Ivan Wills <ivan.wills@gmail.com>
2009-05-16 18:40:10 +1000

    Initial import

---
+++ lib/File/Class.pm
@@ -0,0 +1,107 @@
+package File::Class;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+File::Class - The great new File::Class!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use File::Class;
+
+    my $foo = File::Class->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Ivan Wills, C<< <ivan.wills at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-file-class at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=File-Class>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc File::Class
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=File-Class>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/File-Class>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/File-Class>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/File-Class/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Ivan Wills, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of File::Class
